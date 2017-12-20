55c0c9d912cda88b54943706a6b02cdcfffb539e
fame <fame@fames-MacBook-Pro.local>
2017-12-21 02:47:04 +0700

    Changes animation something somewhere

--- pages/HomeIntro.js
+++ pages/HomeIntro.js
@@ -4,20 +4,17 @@ import styled from 'styled-components'
 import Link from 'next/link'
 import Logo from '../components/Intro/Logo'
 import Button from '../components/Intro/Button'
-import MoveToLeft from '../components/PageTransition/MoveToLeft'
+
 
 
 const Index = () => (
-	
 	<div className ="container-fluid" >      
     <div className="row">
 	<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 		<center><Logo/></center>		
-		<center><Link href="/character"><a><Button style={{width:'7%',marginTop:'1%'}}>ไปผจญภัยกัน!</Button></a></Link></center>
-		
+		<center><Link href="/character"><a><Button style={{width:'10%',marginTop:'1%'}}>ไปผจญภัยกัน!</Button></a></Link></center>
+
     </div>
 	</div></div>
-	
-	
 )
 export default Index
