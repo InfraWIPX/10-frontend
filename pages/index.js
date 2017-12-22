23d81482a8110eb40e713722eff3556f025b490a
fame <fame@fames-MacBook-Pro.local>
2017-12-22 18:23:50 +0700

    fix HomeIntro to component del node 2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // นี่คือหน้า Home นะ
 import React from 'react'
 import Link from 'next/link'
-import HomeIntro from '../pages/HomeIntro'
+import HomeIntro from '../components/Intro/HomeIntro'
 
 
 
