88148abd002caab3c1b426e928ac3001840c0225
fame <fame@fames-MacBook-Pro.local>
2017-12-22 21:53:12 +0700

    เปลี่ยนปุ่มUIขอมาก็จัดปายยยย

--- components/Intro/Button.js
+++ components/Intro/Button.js
@@ -3,15 +3,26 @@ import '../global-style'
 
 const Button = styled.button`
     font-family: 'My custom family';
-    border-radius: 3px;
-    padding: 0.5em ;
-    background-color: transparent;
-    color: #FFA644;
-    border: 2px solid  #FFA644;
+    display: inline-block;
     font-size: 2vw;
+    cursor: pointer;
+    text-align: center;
+    text-decoration: none;
+    outline: none;
+    border-radius: 15px;
+    padding: 0.5em ;
+    background-color: #998A2F;
+    color: #002d40;
+    border: none;
+    box-shadow: 0 0.9vh #999;
+    
     &:hover {
-        background-color: #FFA644; 
-        color: #002D40;
+        background-color: #E4943B; 
+    }
+    &:active{
+        background-color: #E4943B;
+        box-shadow: 0 0.5vh #666;
+        transform: translateY(0.4vh);
     }
 `;
 
