cf45ed862c85645eb6f662b3e40db959140f8dc3
DESKTOP-13DH8UD\papop <papop.ru@mail.kmutt.ac.th>
2017-12-27 02:39:05 +0700

    [creat] หน้าWho หน้า Learner [change] navbar hamburger กับ ปุ่ม ลงทะเบียน ไว้ที่ folder content

--- pages/what-l.js
+++ pages/what-l.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import OurCamp from '../components/OurCamp/HomeOurCamp'
-
-const WhatMonkey =() => (
-<div>    
-    <OurCamp/>       
-</div>
+import HomeLearner from '../components/WhatLearner/HomeLearner'
+import HomeWho from '../components/Who/HomeWho'
+const WhatMonkey = () => (
+    <div>
+        <OurCamp />        
+        <HomeLearner />
+        <HomeWho />
+    </div>
 )
 
 export default WhatMonkey
