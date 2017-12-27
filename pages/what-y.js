6e151267dc25b357e8edecea7f530d61f75077d9
fame <fame@fames-MacBook-Pro.local>
2017-12-28 06:07:22 +0700

    [change] package เปลี่ยนอนิเมชั่นตอนเปลี่ยนหน้า

--- pages/what-y.js
+++ pages/what-y.js
@@ -2,12 +2,15 @@ import React from 'react'
 import OurCamp from '../components/OurCamp/HomeOurCamp'
 import HomeLearner from '../components/WhatLearner/HomeLearner'
 import HomeWho from '../components/Who/HomeWho'
+import FadeD from '../components/PageTransition/Fadein'
 
 const WhatGiant = () => (
+    <FadeD>
     <section>
         <OurCamp />        
         <HomeLearner />
         <HomeWho />
     </section>
+    </FadeD>
 )
 export default WhatGiant
