414c0bec10470d6d560171cc2df21f6a2c1e99c3
mild20091931 <mild20091931@gmail.com>
2018-01-09 17:38:48 +0700

    [Pull]

--- pages/singlepage-l.js
+++ pages/singlepage-l.js
@@ -3,8 +3,10 @@ import OurCamp from '../components/OurCamp/index-L'
 import HomeLearner from '../components/WhatLearner/index-L'
 import Who from '../components/Who/index-L'
 
+
 const SinglepageLing = () => (
     <div>
+
 	<section>
 		<Who/>
 	</section>
